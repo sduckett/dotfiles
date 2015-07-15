cc6dcd01348b4daf7d4a2afbad812d00a3ff2481
Sean Duckett <sduckett@gmail.com>
2015-07-15 10:37:54 -0600

    Try out Thalia's docstrings
    Sort user plugins into tidy bins

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -7,27 +7,40 @@
 ;;; https://github.com/technomancy/leiningen/issues/1900
 
 {:user {:plugins
-        [[org.clojure/tools.nrepl "0.2.10"] ;; https://github.com/clojure/tools.nrepl
-         [chestnut/lein-template "0.8.1"]   ;; https://github.com/plexus/chestnut
-         [lein-midje "3.1.3"]               ;; https://github.com/marick/lein-midje
+        [
+         ;; Middleware for cider
+         [org.clojure/tools.nrepl "0.2.10"] ;; https://github.com/clojure/tools.nrepl
          [cider/cider-nrepl "0.9.1"]        ;; https://github.com/clojure-emacs/cider-nrepl
          [refactor-nrepl "1.1.0"]           ;; https://github.com/clojure-emacs/refactor-nrepl
+
+         ;; Do you feel like RSpec today?
+         [lein-midje "3.1.3"]               ;; https://github.com/marick/lein-midje
+
+         ;; What do I need to fuss about before I commit?
          [lein-bikeshed "0.2.0"]            ;; https://github.com/dakrone/lein-bikeshed
          [lein-deps-tree "0.1.2"]           ;; https://github.com/the-kenny/lein-deps-tree
-         [lein-marginalia "0.8.0"]          ;; https://github.com/gdeer81/lein-marginalia
          [lein-kibit "0.1.2"]               ;; https://github.com/jonase/kibit
          [lein-ancient "0.6.7"]             ;; https://github.com/xsc/lein-ancient
-         [lein-cloverage "1.0.6"]           ;; https://github.com/lshift/cloverage
-         [lein-ubersource "0.1.1"]]         ;; https://github.com/puppetlabs/lein-ubersource
 
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                       [pjstadig/humane-test-output "0.7.0"]]
+         ;; Read the friendly source-code
+         [lein-ubersource "0.1.1"]          ;; https://github.com/puppetlabs/lein-ubersource
+         [thalia "0.1.0"]                   ;; https://github.com/jafingerhut/thalia
+         [lein-marginalia "0.8.0"]          ;; https://github.com/gdeer81/lein-marginalia]
 
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
+         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                        [pjstadig/humane-test-output "0.7.0"]]
 
-        :aliases {"sanity-check" ["do"
-                                  ["clean"]
-                                  ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed"]]}
+         :injections [(require 'pjstadig.humane-test-output)
+                      (pjstadig.humane-test-output/activate!)]
+
+         :aliases {"sanity-check" ["do"
+                                   ["clean"]
+                                   ["with-profile" "production" "deps" ":tree"] ["ancient"] ["kibit"] ["bikeshed"]]
+                   "rtfs" ["do"
+                           ["clean"]
+                           ["ubersource"]
+                           ["marg"]]
+                   }
+         }
         }
  }
