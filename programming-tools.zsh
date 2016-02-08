e263e94634ab01abefd69eeb2aa08d74bff9089b
Sean Duckett <sduckett@gmail.com>
2016-02-07 17:15:11 -0700

    Find dirty git repositories

--- programming-tools.zsh
+++ programming-tools.zsh
@@ -7,3 +7,19 @@ JAVA_PATHS=/usr/local/jdk-1.7.0/bin
 HASKELL_PATHS=$HOME/.cabal/bin
 NODE_PATHS=/usr/local/share/npm/bin
 DEPLOYMENT_PATHS=:/usr/local/heroku/bin
+
+function dirty-repos() {
+    for gitprojpath in `find . -type d -name .git | sed "s/\/\.git//"`; do
+        pushd . >/dev/null
+
+        cd $gitprojpath
+
+        isdirty=$(git status -s | grep "^.*")
+
+        if [ -n "$isdirty" ]; then
+            echo "DIRTY:" $gitprojpath
+        fi
+
+        popd >/dev/null
+    done
+}
