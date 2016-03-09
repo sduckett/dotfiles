c227d12526bdddccac5a9c6a72c7e537d74d07f2
Sean Duckett <sduckett@gmail.com>
2016-03-09 12:02:47 -0700

    the start of an idea

--- democracy-works.zsh
+++ democracy-works.zsh
@@ -1,6 +1,8 @@
 PATH=$PATH:/Users/smd/bin
+# Configure some stuff here
 
-# Configure
 eval $(docker-machine env default)
 
 alias "uplog=krak up --verbose &> kraken.log"
+alias "figgywheel=cd ~/dw/turbovote-web && rlwrap lein do clean, figwheel"
+alias "cleanup-repos=cd /Users/smd/dw; for i in *; do cd $i && git fetch -p && cd -; done"
