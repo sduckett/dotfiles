89c0328419fddd380e00bb5a7f599b3f3cf3b62f
Sean Duckett <sduckett@gmail.com>
2016-06-10 08:46:53 -0600

    `dlet` can be useful for debugging let forms

---
+++ user.clj
@@ -0,0 +1,9 @@
+(ns user)
+
+(defmacro dlet [bindings & body]
+  `(let [~@(mapcat (fn [[n v]]
+                     (if (or (vector? n) (map? n))
+                       [n v]
+                       [n v '_ `(println (name '~n) ":" ~v)]))
+                   (partition 2 bindings))]
+          ~@body))
