aa5ef2012d27f8075b1badb6cc05b3f73972da0c
Sean Duckett <sduckett@gmail.com>
2016-07-05 09:41:17 -0600

    docker for mac is cool now

--- democracy-works.zsh
+++ democracy-works.zsh
@@ -1,7 +1,4 @@
 PATH=$PATH:/Users/smd/bin
-# Configure some stuff here
-
-eval $(docker-machine env default)
 
 alias "uplog=krak up --verbose &> kraken.log"
 alias "figgywheel=cd ~/dw/turbovote-web && rlwrap lein do clean, figwheel"
