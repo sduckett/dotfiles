0a6e835efaf32f36230ed766f34dd2024298e263
Sean Duckett <sduckett@gmail.com>
2016-07-05 09:43:25 -0600

    Emacs config moved to https://github.com/sduckett/emacs-config

--- .emacs.d/baquah-defaults/baquah-defaults-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "baquah-defaults" "0.0.1" "Building a set of personal quirkiness" 'nil)
--- .emacs.d/org-mode-config.el
+++
@@ -1,3 +0,0 @@
-(setq org-directory "~/docs/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map "\C-cr" 'org-capture)
